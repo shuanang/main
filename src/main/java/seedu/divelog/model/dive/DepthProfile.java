f17e0c539aac2480f7126944fad20689f65fb18e
Arjo Chakravarty <arjo129@gmail.com>
2018-10-09 20:33:36 +0800

    Fixed depth profile

--- src/main/java/seedu/divelog/model/dive/DepthProfile.java
+++ src/main/java/seedu/divelog/model/dive/DepthProfile.java
@@ -29,7 +29,7 @@ public class DepthProfile {
 
     @Override
     public boolean equals(Object obj) {
-        if(!(obj instanceof DepthProfile)) {
+        if (!(obj instanceof DepthProfile)) {
             return false;
         }
         DepthProfile other = (DepthProfile) obj;
