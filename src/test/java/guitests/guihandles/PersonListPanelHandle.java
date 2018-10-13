a2c196fc8128daa18d0163914adae28ae3859704
Arjo Chakravarty <arjo129@gmail.com>
2018-10-13 12:45:20 +0800

    fixed PersonListPanelHandle

--- src/test/java/guitests/guihandles/PersonListPanelHandle.java
+++ src/test/java/guitests/guihandles/PersonListPanelHandle.java
@@ -6,19 +6,19 @@ import java.util.Set;
 
 import javafx.scene.Node;
 import javafx.scene.control.ListView;
-import seedu.divelog.model.person.Person;
+import seedu.divelog.model.dive.DiveSession;
 
 /**
  * Provides a handle for {@code DiveListPanel} containing the list of {@code DiveSessionCard}.
  */
-public class PersonListPanelHandle extends NodeHandle<ListView<Person>> {
+public class PersonListPanelHandle extends NodeHandle<ListView<DiveSession>> {
     public static final String PERSON_LIST_VIEW_ID = "#personListView";
 
     private static final String CARD_PANE_ID = "#cardPane";
 
-    private Optional<Person> lastRememberedSelectedPersonCard;
+    private Optional<DiveSession> lastRememberedSelectedDiveCard;
 
-    public PersonListPanelHandle(ListView<Person> personListPanelNode) {
+    public PersonListPanelHandle(ListView<DiveSession> personListPanelNode) {
         super(personListPanelNode);
     }
 
@@ -29,15 +29,15 @@ public class PersonListPanelHandle extends NodeHandle<ListView<Person>> {
      * @throws IllegalStateException if the selected card is currently not in the scene graph.
      */
     public DiveSessionCardHandle getHandleToSelectedCard() {
-        List<Person> selectedPersonList = getRootNode().getSelectionModel().getSelectedItems();
+        List<DiveSession> selectedDiveSessionList = getRootNode().getSelectionModel().getSelectedItems();
 
-        if (selectedPersonList.size() != 1) {
+        if (selectedDiveSessionList.size() != 1) {
             throw new AssertionError("Person list size expected 1.");
         }
 
         return getAllCardNodes().stream()
                 .map(DiveSessionCardHandle::new)
-                .filter(handle -> handle.equals(selectedPersonList.get(0)))
+                .filter(handle -> handle.equals(selectedDiveSessionList.get(0)))
                 .findFirst()
                 .orElseThrow(IllegalStateException::new);
     }
@@ -53,7 +53,7 @@ public class PersonListPanelHandle extends NodeHandle<ListView<Person>> {
      * Returns true if a card is currently selected.
      */
     public boolean isAnyCardSelected() {
-        List<Person> selectedCardsList = getRootNode().getSelectionModel().getSelectedItems();
+        List<DiveSession> selectedCardsList = getRootNode().getSelectionModel().getSelectedItems();
 
         if (selectedCardsList.size() > 1) {
             throw new AssertionError("Card list size expected 0 or 1.");
@@ -65,7 +65,7 @@ public class PersonListPanelHandle extends NodeHandle<ListView<Person>> {
     /**
      * Navigates the listview to display {@code person}.
      */
-    public void navigateToCard(Person person) {
+    public void navigateToCard(DiveSession person) {
         if (!getRootNode().getItems().contains(person)) {
             throw new IllegalArgumentException("Person does not exist.");
         }
@@ -104,12 +104,12 @@ public class PersonListPanelHandle extends NodeHandle<ListView<Person>> {
     public DiveSessionCardHandle getPersonCardHandle(int index) {
         return getAllCardNodes().stream()
                 .map(DiveSessionCardHandle::new)
-                .filter(handle -> handle.equals(getPerson(index)))
+                .filter(handle -> handle.equals(getDive(index)))
                 .findFirst()
                 .orElseThrow(IllegalStateException::new);
     }
 
-    private Person getPerson(int index) {
+    private DiveSession getDive(int index) {
         return getRootNode().getItems().get(index);
     }
 
@@ -125,28 +125,28 @@ public class PersonListPanelHandle extends NodeHandle<ListView<Person>> {
     /**
      * Remembers the selected {@code DiveSessionCard} in the list.
      */
-    public void rememberSelectedPersonCard() {
-        List<Person> selectedItems = getRootNode().getSelectionModel().getSelectedItems();
+    public void rememberSelectedDiveCard() {
+        List<DiveSession> selectedItems = getRootNode().getSelectionModel().getSelectedItems();
 
         if (selectedItems.size() == 0) {
-            lastRememberedSelectedPersonCard = Optional.empty();
+            lastRememberedSelectedDiveCard = Optional.empty();
         } else {
-            lastRememberedSelectedPersonCard = Optional.of(selectedItems.get(0));
+            lastRememberedSelectedDiveCard = Optional.of(selectedItems.get(0));
         }
     }
 
     /**
      * Returns true if the selected {@code DiveSessionCard} is different from the value remembered by the most recent
-     * {@code rememberSelectedPersonCard()} call.
+     * {@code rememberSelectedDiveCard()} call.
      */
     public boolean isSelectedPersonCardChanged() {
-        List<Person> selectedItems = getRootNode().getSelectionModel().getSelectedItems();
+        List<DiveSession> selectedItems = getRootNode().getSelectionModel().getSelectedItems();
 
         if (selectedItems.size() == 0) {
-            return lastRememberedSelectedPersonCard.isPresent();
+            return lastRememberedSelectedDiveCard.isPresent();
         } else {
-            return !lastRememberedSelectedPersonCard.isPresent()
-                    || !lastRememberedSelectedPersonCard.get().equals(selectedItems.get(0));
+            return !lastRememberedSelectedDiveCard.isPresent()
+                    || !lastRememberedSelectedDiveCard.get().equals(selectedItems.get(0));
         }
     }
 
