e142d364715990b667c6c17e76a4804aa988bd01
Ang Yi Shuan <shuanang@hotmail.com>
2018-10-26 20:48:19 +0800

    completed the PressureGroupLogic
    added stub tests as well

--- src/main/java/seedu/divelog/logic/pressuregroup/PressureGroupLogic.java
+++ src/main/java/seedu/divelog/logic/pressuregroup/PressureGroupLogic.java
@@ -1,39 +1,54 @@
 package seedu.divelog.logic.pressuregroup;
 //@@author shuanang
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import static seedu.divelog.commons.util.CompareUtil.readDateFromLong;
+import static seedu.divelog.commons.util.CompareUtil.readTimeFromLong;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import seedu.divelog.commons.util.CompareUtil;
+import seedu.divelog.logic.pressuregroup.exceptions.LimitExceededException;
 import seedu.divelog.model.dive.DepthProfile;
 import seedu.divelog.model.dive.DiveSession;
 import seedu.divelog.model.dive.PressureGroup;
-import seedu.divelog.model.dive.exceptions.InvalidTimeException;
+
 import seedu.divelog.model.divetables.PadiDiveTable;
 
 /**
- * Calculates the pressure group given depth and minutes spent at depth for single/repeat dives.
+ * Calculates the pressure group given depth and minutes spent at depth for single and/or repeat dives.
  */
 public class PressureGroupLogic {
     PressureGroupLogic(){}
     /**
      * Calculates the pressure group given depth and minutes spent at depth for repeat dives.
+     *
+     * @param depth {@code DepthProfile} in metres, the deepest point which the diver descends for the dive
+     * @param actualBottomTime in minutes which the diver spent underwater
+     * @param pg {@code PressureGroup} current PressureGroup object, in consideration for repeat dives
+     * @return new PressureGroup object containing the new pressure group after repeat dive
+     * @throws JSONException If an error occurs during reading of the PADI dive table.
      */
-    public static PressureGroup computePressureGroup(DepthProfile depth,
-                                              float duration, PressureGroup pg) throws JSONException {
+    public static PressureGroup computePressureGroup(DepthProfile depth, float actualBottomTime, PressureGroup pg)
+            throws JSONException, LimitExceededException {
         PadiDiveTable padiDiveTable = PadiDiveTable.getInstance();
         JSONArray arr = padiDiveTable.depthToTimes(depth, pg);
-        assert (int) duration <= arr.getInt(1);
+        int adjustedNoDecompressionLimits = arr.getInt(1);
+        if ((int) actualBottomTime > adjustedNoDecompressionLimits) {
+            throw new LimitExceededException();
+        }
         int totalBottomTime;
-        totalBottomTime = arr.getInt(0) + (int) duration;
+        totalBottomTime = arr.getInt(0) + (int) actualBottomTime;
         PressureGroup newPg;
         newPg = padiDiveTable.depthToPg(depth, totalBottomTime);
         return newPg;
     }
 
     /**
-     * Calculates the pressure group after a given time the user has emerged from the water.
+     * Calculates the pressure group after a given time the diver has emerged from the water.
+     *
+     * @param pg {@code PressureGroup} current PressureGroup object containing the current pressure group
+     * @param duration in minutes: time elapsed after diver has surfaced
+     * @return new PressureGroup object containing the new pressure group after surface interval
      */
     public static PressureGroup computePressureGroupAfterSurfaceInterval(PressureGroup pg, float duration) {
         PadiDiveTable padiDiveTable = PadiDiveTable.getInstance();
@@ -49,30 +64,67 @@ public class PressureGroupLogic {
 
     /**
      * Calculates the time to next pressure group, returns a float with minutes to next pressure group
+     *
+     * @param lastDive {@code DiveSession} current DiveSession object containing the last dive details
+     * @return number of minutes needed to the next immediate Pressure group e.g. "D" to "C"
      */
-    public static PressureGroup computeTimetoNextPg(DiveSession lastDive) throws InvalidTimeException, JSONException {
+    public float computeTimeToNextPg(DiveSession lastDive) throws Exception {
         //fromsurfaceinterval - ending pressure group + difference in minutes -> current pg
-        //To add / clarify : current time cannot be smaller/before the endoflastdivetime
-        String endOfLastDiveTimeString = lastDive.getEnd().getTimeString();
+        long currentDateTime = CompareUtil.getCurrentDateTimeLong(); //ddMMyyyyHHmm
+        String timeNow = readTimeFromLong(currentDateTime);
+        String dateNow = readDateFromLong(currentDateTime);
+        String endOfLastDiveTime = lastDive.getEnd().getTimeString();
+        String endOfLastDiveDate = lastDive.getDateEnd().getOurDateString();
+        long surfaceDuration = CompareUtil.checkTimeDifference(endOfLastDiveTime, timeNow, endOfLastDiveDate, dateNow);
         String endOfLastDivePg = lastDive.getPressureGroupAtEnd().getPressureGroup();
-        String currentTime = new SimpleDateFormat("HH:mm").format(Calendar.getInstance().getTime());
-        String endOfLastDiveTimeFormatted = "".concat(String.valueOf(endOfLastDiveTimeString.charAt(0)))
-                .concat(String.valueOf(endOfLastDiveTimeString.charAt(1))).concat(":")
-                .concat(String.valueOf(endOfLastDiveTimeString.charAt(2)))
-                .concat(String.valueOf(endOfLastDiveTimeString.charAt(3)));
         PadiDiveTable padiDiveTable = PadiDiveTable.getInstance();
-        float currentTimeInt = padiDiveTable.timeToMinutes(currentTime);
-        float endOfLastDiveTimeInt = padiDiveTable.timeToMinutes(endOfLastDiveTimeFormatted);
-        float surfaceDuration = currentTimeInt - endOfLastDiveTimeInt;
+        final int minSurfaceTimeMinutesToMinPressureGroup = 3 * 60;
+        float timeToNextPg;
+        if (surfaceDuration > minSurfaceTimeMinutesToMinPressureGroup) {
+            //"Pressure Group already A"
+            timeToNextPg = 0;
+            return timeToNextPg;
+        }
         String currentPg = computePressureGroupAfterSurfaceInterval(new PressureGroup(endOfLastDivePg), surfaceDuration)
                 .getPressureGroup();
         //from current pg to next pg time : next pg arr[0] - difference in minutes (surfaceDuration)
-        JSONArray arr = padiDiveTable.getSurfaceTable(new PressureGroup(endOfLastDivePg), new PressureGroup(currentPg));
         String nextPg = String.valueOf(((char) (currentPg.charAt(0) - 1)));
-        JSONArray arr1 = padiDiveTable.getSurfaceTable(new PressureGroup(endOfLastDivePg), new PressureGroup(nextPg));
-        String toNextPgMinimumTime = arr1.get(0).toString();
-        float timeToNextPg = padiDiveTable.timeToMinutes(toNextPgMinimumTime) - surfaceDuration;
-        System.out.println("Time to next Pg group, " + nextPg + " : " + timeToNextPg + " minutes");
-        return null;
+        JSONArray nextPgArr = padiDiveTable.getSurfaceTable(new PressureGroup(endOfLastDivePg),
+                new PressureGroup(nextPg));
+        String toNextPgMinimumTime = nextPgArr.get(0).toString();
+        timeToNextPg = padiDiveTable.timeToMinutes(toNextPgMinimumTime) - surfaceDuration;
+        //"Time to next Pg group, " + nextPg + " : " + timeToNextPg + " minutes"
+        return timeToNextPg;
+    }
+
+    /**
+     * Computes time needed to the min Pressure Group "A" from the last dive, with reference to the current time
+     *
+     * @param lastDive {@code DiveSession} current DiveSession object containing the last dive details
+     * @return number of minutes needed to the minimum pressure group "A"
+     */
+    public float computeTimeToMinPressureGroup(DiveSession lastDive) throws Exception {
+        //fromsurfaceinterval - ending pressure group + difference in minutes -> current pg
+        long currentDateTime = CompareUtil.getCurrentDateTimeLong(); //ddMMyyyyHHmm
+        String timeNow = readTimeFromLong(currentDateTime);
+        String dateNow = readDateFromLong(currentDateTime);
+        String endOfLastDiveTime = lastDive.getEnd().getTimeString();
+        String endOfLastDiveDate = lastDive.getDateEnd().getOurDateString();
+        long surfaceDuration = CompareUtil.checkTimeDifference(endOfLastDiveTime, timeNow, endOfLastDiveDate, dateNow);
+        String endOfLastDivePg = lastDive.getPressureGroupAtEnd().getPressureGroup();
+        PadiDiveTable padiDiveTable = PadiDiveTable.getInstance();
+        final int minSurfaceTimeMinutesToMinPressureGroup = 3 * 60;
+        float timeToMinPg;
+        if (surfaceDuration > minSurfaceTimeMinutesToMinPressureGroup) {
+            //"Pressure Group already A"
+            timeToMinPg = 0;
+            return timeToMinPg;
+        }
+        //from current pg to A pressure group min time : getSurfaceTable(last dive pg, "A") arr[0] - surfaceDuration
+        JSONArray toMinPgArr = padiDiveTable.getSurfaceTable(new PressureGroup(endOfLastDivePg),
+                new PressureGroup("A"));
+        String toMinPgTime = toMinPgArr.get(0).toString();
+        timeToMinPg = padiDiveTable.timeToMinutes(toMinPgTime) - surfaceDuration;
+        return timeToMinPg;
     }
 }
--- src/test/java/seedu/divelog/logic/pressuregroup/PressureGroupLogicTest.java
+++ src/test/java/seedu/divelog/logic/pressuregroup/PressureGroupLogicTest.java
@@ -3,8 +3,11 @@ package seedu.divelog.logic.pressuregroup;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.json.JSONException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+import seedu.divelog.logic.pressuregroup.exceptions.LimitExceededException;
 import seedu.divelog.model.dive.DepthProfile;
 import seedu.divelog.model.dive.DiveSession;
 import seedu.divelog.model.dive.Location;
@@ -12,18 +15,34 @@ import seedu.divelog.model.dive.OurDate;
 import seedu.divelog.model.dive.PressureGroup;
 import seedu.divelog.model.dive.Time;
 import seedu.divelog.model.dive.TimeZone;
-import seedu.divelog.model.dive.exceptions.InvalidTimeException;
 
 
 //@@author shuanang
 public class PressureGroupLogicTest {
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
-    public void computePressureGroup_test() throws JSONException {
+    public void computePressureGroup_test() throws JSONException, LimitExceededException {
 
         PressureGroup pressureGroup = PressureGroupLogic.computePressureGroup(new DepthProfile(14),
                 50, new PressureGroup("d"));
         assertEquals(pressureGroup.getPressureGroup(), "T");
+        pressureGroup = PressureGroupLogic.computePressureGroup(new DepthProfile(1),
+                209, new PressureGroup("A"));
+        assertEquals(pressureGroup.getPressureGroup(), "Z");
+        pressureGroup = PressureGroupLogic.computePressureGroup(new DepthProfile(21),
+                10, new PressureGroup("j"));
+        assertEquals(pressureGroup.getPressureGroup(), "P");
+    }
+
+    @Test
+    public void computePressureGroup_actualBottomTimeExceedsNoDecompressionLimitsThrowsException()
+            throws LimitExceededException, JSONException {
+        thrown.expect(LimitExceededException.class);
+        PressureGroup pressureGroup = PressureGroupLogic.computePressureGroup(new DepthProfile(10),
+                210, new PressureGroup("a"));
     }
 
     @Test
@@ -33,17 +52,57 @@ public class PressureGroupLogicTest {
         assertEquals(pressureGroup.getPressureGroup(), "E");
     }
 
-    /*@Test
-    public void computeTimeToNextPg_test() throws InvalidTimeException, JSONException {
-        PressureGroup pressureGroup = PressureGroupLogic.computeTimetoNextPg(new DiveSession(new OurDate("04082018"),
-                new Time("0700"),
-                new Time("0945"),
-                new OurDate("04082018"),
-                new Time("1000"),
+    @Test
+    public void computeTimeToNextPg_stubTest() throws Exception {
+        PressureGroupLogicStub pressureGroupLogicStub = new PressureGroupLogicStub();
+        float timeToNextPg = pressureGroupLogicStub.computeTimeToNextPg(new DiveSession(new OurDate("20102018"),
+                new Time("0916"),
+                new Time("1045"),
+                new OurDate("20102018"),
+                new Time("1100"),
                 new PressureGroup("A"),
-                new PressureGroup("R"),
+                new PressureGroup("R"), //104 minutes at 10m from Pressure Group A to R
                 new Location("Bali"),
                 new DepthProfile(10.0f),
-                new TimeZone("+5")));
-    } */
+                new TimeZone("+8")));
+        assertEquals(timeToNextPg, 8);
+        float timeToNextPg1 = pressureGroupLogicStub.computeTimeToNextPg(new DiveSession(new OurDate("19102018"),
+                new Time("0916"),
+                new Time("1045"),
+                new OurDate("19102018"), //dive ended 1 day before and diver checks
+                new Time("1100"),
+                new PressureGroup("A"),
+                new PressureGroup("R"), //104 minutes at 10m from Pressure Group A to R
+                new Location("Bali"),
+                new DepthProfile(10.0f),
+                new TimeZone("+8")));
+        assertEquals(timeToNextPg1, 0);
+    }
+    @Test
+    public void computeTimeToMinPressureGroup_stubTest() throws Exception {
+        PressureGroupLogicStub pressureGroupLogicStub = new PressureGroupLogicStub();
+        float timeToMinPg = pressureGroupLogicStub.computeTimeToMinPressureGroup(new DiveSession(
+                new OurDate("20102018"),
+                new Time("0916"),
+                new Time("1045"),
+                new OurDate("20102018"),
+                new Time("1100"),
+                new PressureGroup("A"),
+                new PressureGroup("R"), //104 minutes at 10m from Pressure Group A to R
+                new Location("Bali"),
+                new DepthProfile(10.0f),
+                new TimeZone("+8")));
+        assertEquals(timeToMinPg, 95);
+        float timeToMinPg1 = pressureGroupLogicStub.computeTimeToNextPg(new DiveSession(new OurDate("19102018"),
+                new Time("0916"),
+                new Time("1045"),
+                new OurDate("19102018"), //dive ended 1 day before and diver checks
+                new Time("1100"),
+                new PressureGroup("A"),
+                new PressureGroup("R"), //104 minutes at 10m from Pressure Group A to R
+                new Location("Bali"),
+                new DepthProfile(10.0f),
+                new TimeZone("+8")));
+        assertEquals(timeToMinPg1, 0);
+    }
 }
