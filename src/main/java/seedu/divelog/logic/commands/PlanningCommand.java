9f8e2a3e41224a95130cb3692ed1c914f9239fb7
cjunxiang <e0176013@u.nus.edu>
2018-10-30 17:16:46 +0800

    UG updates

--- src/main/java/seedu/divelog/logic/commands/PlanningCommand.java
+++ src/main/java/seedu/divelog/logic/commands/PlanningCommand.java
@@ -11,8 +11,8 @@ import seedu.divelog.model.Model;
  */
 public class PlanningCommand extends Command {
 
-    public static final String COMMAND_WORD_PLAN = "Newplan";
-    public static final String COMMAND_WORD_NORMAL = "Normalmode";
+    public static final String COMMAND_WORD_PLAN = "newplan";
+    public static final String COMMAND_WORD_NORMAL = "normalmode";
 
     public static final String MESSAGE_ENTRY = "You're now in Planning Mode";
     public static final String MESSAGE_EXIT = "You're now in Normal Mode";
